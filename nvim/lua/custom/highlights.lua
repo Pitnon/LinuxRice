1e1c127ce32bebcce18dfd4155b3b003c515c610
Pitnon <102268834+Pitnon@users.noreply.github.com>
2023-02-22 16:07:44 -0500

    Delete nvim directory

--- nvim/lua/custom/highlights.lua
+++
@@ -1,8 +0,0 @@
-local M = {
-    Comment = { italic = true },
-    -- NvimTreeFolderName = { italic = true },
-    NvimTreeExecFile = { bold = false },
-    -- NvimTreeGitDirty = { bold = false },
-}
-
-return M
